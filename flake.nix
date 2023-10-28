dda0869a1dcbd7888509d922eacfaf70e78c1898
Lea Waller <lea@lea.science>
2023-10-28 18:46:16 +0200

    Create editable install in direnv

--- flake.nix
+++ flake.nix
@@ -3,27 +3,59 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pyproject.url = "github:nix-community/pyproject.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pyproject }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (nixpkgs) lib;
+
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
-        pythonPackages = with pkgs.python311Packages; [
-          dropbox
-          more-itertools
-          pip
-          pyyaml
-          sqlalchemy
-          tqdm
-          types-pyyaml
-        ];
+
+        python = pkgs.python311.withPackages (py:
+          with py; [
+            dropbox
+            more-itertools
+            pip
+            pyyaml
+            sqlalchemy
+            tqdm
+            types-pyyaml
+          ]);
+
+        # Loads pyproject.toml into a high-level project representation
+        project = pyproject.lib.project.loadPyproject {
+          pyproject = lib.importTOML ./pyproject.toml;
+        };
+        objectReferenceToModule = objectReference:
+          builtins.elemAt (lib.splitString ":" objectReference) 0;
+        objectReferenceToAttr = objectReference:
+          builtins.elemAt (lib.splitString ":" objectReference) 1;
+        scripts = (lib.mapAttrsToList (name: objectReference:
+          (pkgs.writeScriptBin name ''
+            #!${python.interpreter}
+            # -*- coding: utf-8 -*-
+            import re
+            import sys
+            from ${objectReferenceToModule objectReference} import ${
+              objectReferenceToAttr objectReference
+            }
+            if __name__ == "__main__":
+                sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+                sys.exit(${objectReferenceToAttr objectReference}())
+          '')) project.pyproject.project.scripts);
+
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ python311 ] ++ pythonPackages;
+          buildInputs = [ python ] ++ scripts;
+          shellHook = ''
+            PYTHONPATH=$(git rev-parse --show-toplevel)/src:$PYTHONPATH
+            export PYTHONPATH
+          '';
         };
       });
 }
